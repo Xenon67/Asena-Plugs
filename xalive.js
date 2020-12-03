ad55ededb70c1005e7b90bb55e5076c07a302dbd
Baran <57062330+Xenon67@users.noreply.github.com>
2020-12-03 13:46:06 +0300

    Add files via upload

---
+++ xalive.js
@@ -0,0 +1,12 @@
+/* Copyright (C) 2020 Yusuf Usta.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+WhatsAsena - Yusuf Usta
+*/
+
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+Asena.addCommand({pattern: 'xalive', fromMe: true}, (async (message, match) => {
+    return await message.sendMessage('_Emrindeyim sahip ⚝ Asena DevXenon için çalışıyor!_\n\n_Developed by *DevXenon*\n\n*Moderatör:*\n\nhttps://github.com/Xenon67/');
+}));
