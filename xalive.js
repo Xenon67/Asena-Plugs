ad78dc5e68cd529f2321dbbef54b3a37e3e70fdc
Baran <57062330+Xenon67@users.noreply.github.com>
2020-12-03 13:26:12 +0300

    Add files via upload

---
+++ xalive.js
@@ -0,0 +1,13 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+const {spawnSync} = require('child_process');
+const Config = require('../config');
+
+const Language = require('../language');
+const Lang = Language.getString('system_stats');
+
+Asena.addCommand({pattern: 'xalive', fromMe: true, (async (message, match) => {
+    await message.sendMessage(
+        '```Emrindeyim sahip ⚝ Asena @DevXenon için çalışıyor!```\n\n*Version:* ```'+Config.VERSION+'```\n*Moderatör:* https://github.com/Xenon67\n*Codded by Xenon*' , MessageType.text
+    );
+}));
