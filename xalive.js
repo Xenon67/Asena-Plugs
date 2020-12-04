343a17a0adf8cebd4b68066fd93fafd787c11125
Baran <57062330+Xenon67@users.noreply.github.com>
2020-12-05 00:22:26 +0300

    Update xalive.js

--- xalive.js
+++ xalive.js
@@ -8,5 +8,5 @@ const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 
 Asena.addCommand({pattern: 'xalive', fromMe: true}, (async (message, match) => {
-    return await message.sendMessage('_Emrindeyim sahip ⚝ Asena DevXenon için çalışıyor!_\n\n```Codded by DevXenon```\n\n*Projeler:*\n\nhttps://github.com/Xenon67');
+    return await message.sendMessage('_Emrindeyim sahip_ ⚝ _Asena DevXenon için çalışıyor!_\n\n```Codded by DevXenon```\n\n*Projeler:*\n\nhttps://github.com/Xenon67');
 }));
