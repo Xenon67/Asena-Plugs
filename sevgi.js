ba6bd810845126d34ce719e6626e5410c152c11a
Baran <57062330+Xenon67@users.noreply.github.com>
2020-12-05 02:52:42 +0300

    Update sevgi.js

--- sevgi.js
+++ sevgi.js
@@ -3,18 +3,19 @@ const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 
 Asena.addCommand({pattern: 'sevgi', fromMe: true}, (async (message, match) => {
+
     return await message.sendMessage('_Seni_');
     
     function myfonction() { ... }
 
 setTimeout(myfonction, 5000); 
 
-    await message.sendMessage('_Çok_')
+    return await message.sendMessage('_Çok_')
     
     function myfonction() { ... }
 
 setTimeout(myfonction, 5000); 
 
-    await message.sendMessage('_Seviyorum_')
+    return await message.sendMessage('_Seviyorum_')
     
 }));
