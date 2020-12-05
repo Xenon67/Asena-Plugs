2e2e82b330ca1bc9419b566bbd64dcce895dad12
Baran <57062330+Xenon67@users.noreply.github.com>
2020-12-05 14:07:40 +0300

    Create ask.js

---
+++ ask.js
@@ -0,0 +1,18 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+Asena.addCommand({pattern: 'ask', fromMe: true}, (async (message, match) => {
+    
+    await message.sendMessage('*Seni*'); 
+
+    var delayInMilliseconds = 1000;
+    setTimeout(function() {
+    await message.sendMessage('*Çok*')
+
+}, delayInMilliseconds);
+
+    setTimeout(function() {
+    await message.sendMessage('*Seviyorum*')
+}, delayInMilliseconds);
+
+}));
