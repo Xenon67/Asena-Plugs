ba10d20ae56ae3a889ae793b41200481930b1932
Baran <57062330+Xenon67@users.noreply.github.com>
2020-12-11 20:42:27 +0300

    Create THANOS.js

---
+++ THANOS.js
@@ -0,0 +1,14 @@
+const Asena = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+
+Asena.addCommand({pattern: 'thanosoz' , fromMe: true , desc: '*Codded by @DevXenon • IG: kyrie.baran*'}, (async (message, match) => {
+
+var text_st = new Array("*Ben Kaçınılmazım!*", "*İşim bittiğinde, insanlığın yarısı hala var olacak. Her şeyin olması gerektiği gibi mükemmel dengelenmiş. Umarım seni hatırlarlar.*);
+
+var l = text_st.length;
+
+var rnd_no = Math.floor(l*Math.random());
+
+await event.SenMessage('text_st');
+
+})));
