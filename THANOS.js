3563adf63b52d032d73f47e6a1335684a6f90f7a
Baran <57062330+Xenon67@users.noreply.github.com>
2020-12-11 20:47:11 +0300

    Update THANOS.js

--- THANOS.js
+++ THANOS.js
@@ -1,7 +1,7 @@
 const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 
-Asena.addCommand({pattern: 'thanosoz' , fromMe: true , desc: '*Codded by @DevXenon • IG: kyrie.baran*'}, (async (message, match) => {
+Asena.addCommand ({pattern: 'thanosoz' , fromMe: true , desc: '*Codded by @DevXenon • IG: kyrie.baran*'}, (async (message, match) => {
 
 var text_st = new Array("*Ben Kaçınılmazım!*", "*İşim bittiğinde, insanlığın yarısı hala var olacak. Her şeyin olması gerektiği gibi mükemmel dengelenmiş. Umarım seni hatırlarlar.*);
 
